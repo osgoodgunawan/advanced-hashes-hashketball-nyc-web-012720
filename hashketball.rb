10c8b992c2052a6b9009aa6c175d7f1a5fcd475e
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-22 01:47:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -300,7 +300,7 @@ def player_with_longest_name
   array=[]
   game_hash.each do |home_away, the_rest|
     the_rest[:players].each do |counter|
-      array << counter[:player_name]
+      array.push(counter[:player_name])
   end
 end
  array.max_by {|max| max.length}
